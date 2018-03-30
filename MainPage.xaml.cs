7a14f2318a7252ff085ec87863f84b9cf1d8fc7a
xfangfang <2553041586@qq.com>
2018-03-30 21:10:06 +0800

    加入简单播放器

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +26,54 @@ namespace NetEasePlayer_UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Binding binding;
         public MainPage()
         {
             this.InitializeComponent();
+  
+        }
+
+
+
+        // https://docs.microsoft.com/zh-cn/uwp/api/windows.storage.pickers.fileopenpicker#examples
+        
+        private async System.Threading.Tasks.Task openFileDialogAsync()
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".mp3");
+            openPicker.FileTypeFilter.Add(".mp4");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                //text_file.Text = "Picked photo: " + file.Name;
+                // https://blog.csdn.net/linwh8/article/details/70314698
+                video_player.Source = MediaSource.CreateFromStorageFile(file);
+                video_player.Visibility = Visibility.Visible;
+                button_open.Visibility = Visibility.Collapsed;
+                // https://docs.microsoft.com/zh-cn/uwp/api/windows.ui.xaml.controls.mediaplayerelement#handle-media-events
+                // http://www.runoob.com/csharp/csharp-event.html
+                video_player.MediaPlayer.MediaEnded += new TypedEventHandler<Windows.Media.Playback.MediaPlayer, object>((player,obj)=> {
+                    // https://www.cnblogs.com/jiahuafu/p/5478695.html
+                    button_open.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,() => {
+                        button_open.Visibility = Visibility.Visible;
+                        video_player.Visibility = Visibility.Collapsed;
+                    });
+                });
+            } 
+            else
+            {
+                text_file.Text = "未选择文件";
+            }
+        }
+
+    
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             openFileDialogAsync();
         }
     }
 }
